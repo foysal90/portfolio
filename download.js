f53015678792a2d0937c44bdf512aa46b9580f6d
foysal90 <foysal1416@hotmail.com>
2023-07-27 23:23:34 -0400

    cv addded

--- download.js
+++ download.js
@@ -3,4 +3,9 @@ downEle.addEventListener("click", () => {
   const span = document.getElementById("span");
   downEle.style.backgroundColor = "red !important;";
   downEle.style.color = "red";
+  span.style.visibility = "visible";
+  setTimeout(()=>{
+    span.style.visibility ="hidden";
+
+  }, 3000)
 });
